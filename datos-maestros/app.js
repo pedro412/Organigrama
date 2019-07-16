456585d9229eb77d88c4b65976c717147b525548
pedro412 <pedro.alvarez412@outlook.com>
2019-07-16 09:03:41 -0500

    greend background, org structure and details

--- datos-maestros/app.js
+++ datos-maestros/app.js
@@ -4,80 +4,76 @@ var data = {
   img: '../datos-maestros/img/elsa-rodriguez.jpg',
   children: [
     {
-      puesto: 'Subdirección',
+      puesto: 'Subdirección de Datos Maestros',
       colaborador: 'Irma Palacios',
       img: '../datos-maestros/img/irma-palacios.jpg',
       detalles:
-        'Subdireccion de Datos Maestros: Responsable de brindar orientación con el fin de lograr los objetivos del negocio, organizar y administrar los recursos del área, controlar y regular los procesos',
+        'Subdireccion de Datos Maestros: Responsable de brindar orientación con el fin de lograr los objetivos del negocio, organizar y administrar los recursos del área, controlar y regular los procesos.',
       children: [
         {
-          puesto: 'Gobierno Datos',
-          colaborador: 'Javier Baca',
-          img: '../datos-maestros/img/javier-baca.jpg',
-          detalles:
-            'Gobierno de Datos:  Responsable de la actualización a políticas, procedimientos y manuales, revisiones al personal para el  correcto cumplimiento de procesos primer interacción con TI y nuevos proyectos.'
-        },
-        {
-          puesto: 'Gerente',
+          puesto: 'Gerente de Datos Maestros',
           colaborador: 'Yolanda González',
           img: '../datos-maestros/img/yolanda-gonzalez.jpg',
           detalles:
-            'Gerente de Datos Maestros: Responsable de coordinar al equipo a cargo, realizar evaluaciones continuas, dar continuidad a los proyectos de negocio con participacion de  Datos Maestros.',
+            'Responsable de coordinar al equipo Comercial, Administrativo y Logístico, realizar evaluaciones, revisiones al correcto cumplimiento de las tareas del equipo a cargo, dar continuidad a los proyectos de negocio con participación de Datos Maestros.',
           children: [
             {
-              puesto: 'Jefatura Datos Maestros',
+              puesto: 'Jefatura de Datos Maestros',
               colaborador: 'Patricia Gutiérrez',
               img: '../datos-maestros/img/patricia-gutierrez.jpg',
               detalles:
-                'Jefe de Datos Maestros: Coordinar y administrar la información de los catálogos maestros (productos, proveedores y relacionados) mediante el registro y la actualización de movimientos, garantizando que cuenten con la información correcta en los sistemas periféricos y sistemas satélites, de Farmacias de Similares y Farmacias del Dr. Simi (Chile), con el fin de contribuir en la correcta operación del negocio',
+                'Coordinar y administrar la informacion del equipo Comercial (maestro de articulos y precios) y Administrativo (proveedores y organigramas de venta) mediante la revision, registro y actualización de movimientos, garantizando la correcta información en los sistemas.',
               children: [
                 {
-                  puesto: 'Asesor Datos Maestros \n Comercial',
+                  puesto: 'Asesor Comercial',
+                  colaborador: 'Jóse Carlos Bermúdez',
+                  img: '../datos-maestros/img/jose-carlos-bermudez.jpg',
                   detalles:
-                    'Área Comercial : Responsable del Maestro de Artículos,  Precios (clases de condición y listas inclusión/exclusión) y Promociones (Set de Ventas).',
-                  children: [
-                    {
-                      puesto: 'Analista B',
-                      colaborador: 'Jóse Carlos Bermúdez',
-                      img: '../datos-maestros/img/jose-carlos-bermudez.jpg'
-                    }
-                  ]
+                    'Responsable de la operación del  catálogo maestro de artículos.'
                 },
                 {
-                  puesto: 'Asesor Datos Maestros \n Administrativo',
+                  puesto: 'Analista',
+                  colaborador: 'Erika Guadarrama',
+                  img: '../datos-maestros/img/erika-guadarrama.jpg',
                   detalles:
-                    'Área Administrativa : Responsable del Maestro de Proveedores,  Clientes Mayoreo y Gestión de catálogos de internacional para Chile  (Artículos, unidades, proveedores y clientes)',
-                  children: [
-                    {
-                      puesto: 'Analista C',
-                      colaborador: 'Erika Guadarrama',
-                      img: '../datos-maestros/img/erika-guadarrama.jpg'
-                    },
-                    {
-                      puesto: 'Analista C',
-                      colaborador: 'Noemi Carreño',
-                      img: '../datos-maestros/img/noemi-carreño.jpg'
-                    }
-                  ]
+                    'Responsable de la operación del  catálogo maestro de proveedores.'
+                },
+                {
+                  puesto: 'Asesor Administrativo',
+                  colaborador: 'Noemi Carreño',
+                  img: '../datos-maestros/img/noemi-carreño.jpg',
+                  detalles:
+                    'Responsable de la operación de los catálogos maestros de Farmacias del Dr. Simi, Chile.'
                 }
               ]
             },
             {
-              puesto: 'Asesor Datos Maestros \n Logística',
+              puesto: 'Gobierno de Datos Maestros',
+              colaborador: 'Javier Baca',
+              img: '../datos-maestros/img/javier-baca.jpg',
+              detalles:
+                'Gobierno de Datos:  Responsable de la actualización a políticas, procedimientos y manuales, revisiones al personal para el  correcto cumplimiento de procesos; primer interacción con TI y nuevos proyectos.'
+            },
+            {
+              puesto: 'Asesor de Datos Maestros Logística',
               colaborador: 'Hugo Sánchez',
               img: '../datos-maestros/img/hugo-sanchez.jpg',
               detalles:
-                'Área Logística: Responsable del Maestro de centros (unidades de negocio), Maestro clientes franquicias e Interlocutores.',
+                'Coordinar y administrar la información del equipo Logístico (maestro de  centros/unidades de negocio y maestro clientes), mediante las revisión, registro y actualización de movimientos, garantizando la correcta información en los sistemas.',
               children: [
                 {
-                  puesto: 'Analista C',
+                  puesto: 'Analista',
                   colaborador: 'Magdalena Gutiérrez',
-                  img: '../datos-maestros/img/magdalena-gutierrez.jpg'
+                  img: '../datos-maestros/img/magdalena-gutierrez.jpg',
+                  detalles:
+                    'Responsable de los registros en los sistemas en el maestro de clientes.'
                 },
                 {
-                  puesto: 'Analista C',
+                  puesto: 'Analista',
                   colaborador: 'Viridiana Contreras',
-                  img: '../datos-maestros/img/viridiana-contreras.jpg'
+                  img: '../datos-maestros/img/viridiana-contreras.jpg',
+                  detalles:
+                    'Responsable de los registros en los sistemas en el maestro de centros.'
                 }
               ]
             }
@@ -97,7 +93,7 @@ var svg = d3
 var transform = d3.zoomIdentity;
 var gContainer = svg.append('g');
 var dataStructure = d3.hierarchy(data);
-var treeLayout = d3.tree().size([1500, 500]);
+var treeLayout = d3.tree().size([2000, 500]);
 var information = treeLayout(dataStructure);
 
 var div = d3
@@ -114,7 +110,7 @@ var connections = gContainer
 connections
   .enter()
   .append('path')
-  .attr('transform', 'translate(-220, 60)')
+  .attr('transform', 'scale(0.60)translate(-150, 90)')
   .attr('d', function(d) {
     return (
       'M' +
@@ -137,15 +133,15 @@ rectangles
   .enter()
   .append('rect')
   .attr('fill', '#15202b')
-  .attr('width', '170px')
+  .attr('width', '240px')
   .attr('height', '50px')
   .attr('x', function(d) {
-    return d.x - 85;
+    return d.x - 120;
   })
   .attr('y', function(d) {
     return d.y - 25;
   })
-  .attr('transform', 'translate(-220, 60)')
+  .attr('transform', 'scale(0.60)translate(-150, 90)')
   .on('mouseover', function(d) {
     div
       .transition()
@@ -167,41 +163,28 @@ rectangles
         }
 
         if (imgColaborador == undefined) {
-          return `<div class="colaborador-template">
-        <div>
-            <h2 class="modal-header">${colaborador}</h2>
-            <p><strong class="modal-puesto">${d.data.puesto}</strong></p>
-          </div>
-        </div>
-        <div class="modal-content">
-          ${detalles}
-        </div>`;
+          return '<div class="colaborador-template"><div><h2 class="modal-header">'
+            .concat(colaborador, '</h2><p><strong class="modal-puesto">')
+            .concat(
+              d.data.puesto,
+              '</strong></p></div></div><div class="modal-content">'
+            )
+            .concat(detalles, '</div>');
         }
 
-        return `<div class="colaborador-template">
-            <img class="img-modal" src="${imgColaborador}">
-          <div>
-              <h2 class="modal-header">${colaborador}</h2>
-              <p><strong class="modal-puesto">${d.data.puesto}</strong></p>
-            </div>
-          </div>
-          <div class="modal-content">
-            ${detalles}
-          </div>
-          `;
+        return '<div class="colaborador-template"><img class="img-modal" src="'
+          .concat(imgColaborador, '"><div><h2 class="modal-header">')
+          .concat(colaborador, '</h2><p><strong class="modal-puesto">')
+          .concat(
+            d.data.puesto,
+            '</strong></p></div></div><div class="modal-content">'
+          )
+          .concat(detalles, '</div>');
       })
       .style('left', d3.event.pageX + 'px')
       .style('top', d3.event.pageY - 28 + 'px');
-    // return d3.select('#details').html(function() {
-    //   if (d.data) {
-    //     openModal(d);
-    //   } else {
-    //     d3.select('#details').style('visibility', 'visible');
-    //   }
-    // });
   })
   .on('mouseleave', function() {
-    // d3.select('#details').style('visibility', 'hidden');
     div
       .transition()
       .duration(500)
@@ -222,7 +205,55 @@ names
     }
     return d.data.puesto;
   })
-  .attr('transform', 'translate(-220, 60)')
+  .attr('transform', 'scale(0.60)translate(-150, 90)')
+  .on('mouseover', function(d) {
+    div
+      .transition()
+      .duration(200)
+      .style('opacity', 1);
+    div
+      .html(function() {
+        var colaborador = d.data.colaborador;
+        var imgColaborador = d.data.img;
+
+        var detalles = d.data.detalles;
+
+        if (colaborador == undefined) {
+          colaborador = '';
+        }
+
+        if (detalles == undefined) {
+          detalles = '';
+        }
+
+        if (imgColaborador == undefined) {
+          return '<div class="colaborador-template"><div><h2 class="modal-header">'
+            .concat(colaborador, '</h2><p><strong class="modal-puesto">')
+            .concat(
+              d.data.puesto,
+              '</strong></p></div></div><div class="modal-content">'
+            )
+            .concat(detalles, '</div>');
+        }
+
+        return '<div class="colaborador-template"><img class="img-modal" src="'
+          .concat(imgColaborador, '"><div><h2 class="modal-header">')
+          .concat(colaborador, '</h2><p><strong class="modal-puesto">')
+          .concat(
+            d.data.puesto,
+            '</strong></p></div></div><div class="modal-content">'
+          )
+          .concat(detalles, '</div>');
+      })
+      .style('left', d3.event.pageX + 'px')
+      .style('top', d3.event.pageY - 28 + 'px');
+  })
+  .on('mouseleave', function() {
+    div
+      .transition()
+      .duration(500)
+      .style('opacity', 0);
+  })
   .attr('x', function(d) {
     return d.x;
   })
